54eaf971832e41ecc07bb05c8d44e42fc71d0b81
Manon Hassna <m.hassnaoui@ludus-academie.com>
2017-03-22 17:42:10 +0100

    commit - test

--- NGL_studio_Manon_Hassnaoui/app/src/main/java/com/example/manon/ngl_studio_manon_hassnaoui/MainActivity.java
+++ NGL_studio_Manon_Hassnaoui/app/src/main/java/com/example/manon/ngl_studio_manon_hassnaoui/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.manon.ngl_studio_manon_hassnaoui;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+//OUHOU
 public class MainActivity extends AppCompatActivity {
 
     @Override
